a8d523d17edae84e29fcbedba09c06b7d12cfa7d
Sabre Alam <142332803+sabrealam@users.noreply.github.com>
2024-05-02 01:00:27 +0530

    a

--- route/user.route.js
+++ route/user.route.js
@@ -34,4 +34,14 @@ route.post("/login", async (req, res) => {
   }
 });
 
+route.get("/", auth, async (req, res) => {
+    try {
+      let users = await UserModel.find();
+      if(users.length == 0) return res.status(400).send({message : "No Users Found"})
+      res.status(200).send({data : users})
+    } catch (error) {
+      res.status(400).send({message : error.message})
+    }
+})
+
 module.exports = route;
